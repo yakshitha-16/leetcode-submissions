6f0c65bce64356861fbdd12f547ef3df3bebe87e
yakshitha-16 <128157047@sastra.ac.in>
2026-08-23 11:16:44 +0530

    Time: 0 ms (100%), Space: 9.8 MB (26.1%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        //anagram = same freq of letters 
+        unordered_map<char , int > mp ;
 
-        if(s.length()!=t.length())
-        return false ;
-        
-        // sort(s.begin() , s.end());
-        // sort(t.begin() , t.end());
+        if(s.size() != t.size())
+            return false;
 
-        // return s==t ;
 
-        int freq[26] = {0};
-
-        for(int i=0;i<s.length();i++)
-        {
-            freq[s[i]-'a']++ ;
-            freq[t[i]-'a']-- ;
+        for(int i=0 ;i<s.size() ; i++){
+            mp[s[i]]++ ;
         }
 
-        for(int i=0;i<26;i++)
-        {
-            if(freq[i]!=0)
-            return false ;
+        for(int i=0 ;i<t.size() ; i++){
+            mp[t[i]]-- ;
+
+            if(mp[t[i]] < 0)
+                return false ;
         }
 
         return true ;
+
+
+
+
+
 
     }
 };
