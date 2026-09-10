e6e1e03914324c429082b8996f643efbf0116aa5
yakshitha-16 <128157047@sastra.ac.in>
2026-09-10 09:21:44 +0530

    Time: 1 ms (43.04%), Space: 10 MB (52.51%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -2,26 +2,21 @@ class Solution {
 public:
     bool isPalindrome(string s) {
         int i = 0 ;
-        int j = s.size()-1;
+        int j = s.size() -1 ;
 
-        //two pointers
-
-        //preprocess 
         while(i<j){
             while(i<j && !isalnum(s[i]))
                 i++ ;
-
             while(i<j && !isalnum(s[j]))
                 j-- ;
 
             if(tolower(s[i]) != tolower(s[j]))
                 return false ;
-
+            
             i++ ;
             j-- ;
 
         }
-
         return true ;
     }
 };
